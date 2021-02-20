b010e440cd6a53d63569889cd008e0586a0f3d0f
sooyoung-h <sooyoung.h8@gmail.com>
2021-02-20 15:05:28 +0900

    JS : scroll (dataset,이벤트 위임)

--- main.js
+++ main.js
@@ -12,43 +12,18 @@ document.addEventListener('scroll', ()=>{
 });
 
 //2. menu click -> move 내 방식으로 했을 경우
-const btn__home = document.querySelector('#btn__home');
-const btn__about = document.querySelector('#btn__about');
-const btn__skills = document.querySelector('#btn__skills');
-const btn__work = document.querySelector('#btn__work');
-const btn__test = document.querySelector('#btn__test');
-const btn__contact = document.querySelector('#btn__contact');
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=>{
+    //! 이벤트 위임 !
+    // event.target은 실제 클릭된 애 (여기선 home, skills, work 등)
+    // event.currentTarget은 이벤트가 걸려있는 element (여기선 navbarmenu)
+    const target = event.target;
+    const link = target.dataset.link;
 
-const home = document.querySelector('#home');
-const about = document.querySelector('#about');
-const skills = document.querySelector('#skills');
-const work = document.querySelector('#work');
-const testimonials = document.querySelector('#testimonials');
-const contact = document.querySelector('#contact');
-
-//이벤트 위임!
-    const navbar__menu = document.querySelector('.navbar__menu');
-
-    navbar__menu.addEventListener( 'click', (event)=> {
-
-    const homeHeight = home.getBoundingClientRect().height -navbarHeight;
-    const aboutHeight = homeHeight + about.getBoundingClientRect().height;
-    const skillHeight = aboutHeight + skills.getBoundingClientRect().height;
-    const workHeight = skillHeight + work.getBoundingClientRect().height;
-    const testimonialsHeight = workHeight + skills.getBoundingClientRect().height;
-    //위의 변수들을 전역변수로 설정하면 오류가 남-> 질문!
-
-    if (event.target == btn__home) {
-        window.scroll(0,0);
-    } else if (event.target == btn__about) {
-        window.scroll(0,homeHeight);
-    } else if (event.target == btn__skills) {
-        window.scroll(0,aboutHeight);
-    } else if (event.target == btn__work) {
-        window.scroll(0,skillHeight);
-    } else if (event.target == btn__test) {
-        window.scroll(0,workHeight);
-    } else if (event.target == btn__contact) {
-        window.scroll(0,testimonialsHeight);
-    } 
+    if (link == null) {
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    //link는 #about이면 id가 about인 element를 찾음
+    scrollTo.scrollIntoView({behavior:"smooth"});
 });
