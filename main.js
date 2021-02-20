b58a505f3f7b1da84877ee44e7d433ac911eb352
sooyoung-h <sooyoung.h8@gmail.com>
2021-02-20 22:54:52 +0900

    JS : Filtering
    dataset 사용

--- main.js
+++ main.js
@@ -70,3 +70,24 @@ document.addEventListener("scroll", () => {
 arrowBtn.addEventListener("click", () => {
   home.scrollIntoView({ bahavior: "smooth" });
 });
+
+//5. Project filtering & animation
+const workBtnContainer = document.querySelector(".work__categories");
+const projectBtnContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+
+workBtnContainer.addEventListener("click", (event) => {
+  const filter =
+    event.target.dataset.filter || event.target.parentNode.dataset.filter;
+
+  if (filter == null) {
+    return;
+  }
+  projects.forEach((project) => {
+    if (filter === "*" || filter === project.dataset.type) {
+      project.classList.remove("invisible");
+    } else {
+      project.classList.add("invisible");
+    }
+  });
+});
